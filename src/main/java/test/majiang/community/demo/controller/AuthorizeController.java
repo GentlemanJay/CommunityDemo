ea57d22b44fdc395f025bbf5326fb9cc5ad8a3ce
GentlemanJay <xsj1996@qq.com>
2019-12-28 16:16:20 +0800

    自动读取配置文件

--- src/main/java/test/majiang/community/demo/controller/AuthorizeController.java
+++ src/main/java/test/majiang/community/demo/controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package test.majiang.community.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,15 +22,26 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+
+    /**
+     * 通过@Value注解自动获取配置文件中的属性值并赋值给相应的变量
+     */
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("a30fee9101e3e694b1f2");
-        accessTokenDTO.setClient_secret("4fc50afb8b7290969c23d9939f563596bb724e7d");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         System.out.println("----accessToken: " + accessToken);
